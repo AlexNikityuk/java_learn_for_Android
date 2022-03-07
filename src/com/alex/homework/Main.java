0c6f3631dad2a5cdbcd328d3666463a6de0933ad
AlexNikityuk <nikityuk1212@gmail.com>
2022-03-07 22:47:18 +0200

    Задание 1

---
+++ src/com/alex/homework/Main.java
@@ -0,0 +1,17 @@
+package com.alex.homework;
+
+public class Main {
+    public static void main(String[] args) {
+        byte first_number = 122;
+        short second_number = 12235;
+        int third_number = 1223312312;
+        long fourth_number = 1544545412123L;
+        float fifth_number = 12245.010f;
+        double sixth_number = 12545456.2211584;
+        char symbol = 'c';
+        boolean isTrue = true;
+        String name = "Alex";
+    }
+
+
+}
